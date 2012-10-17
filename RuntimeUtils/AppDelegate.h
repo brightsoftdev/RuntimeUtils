6b615d3613e3fb3f8cf5c73abf7a5d555078be94
yangzexin <yang380065007@hotmail.com>
2012-10-17 16:06:36 +0800

    Initial Commit

---
+++ RuntimeUtils/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RuntimeUtils
+//
+//  Created by yangzexin on 12-10-17.
+//  Copyright (c) 2012年 gewara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
